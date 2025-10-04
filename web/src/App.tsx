1f988d606a61f8651af8584df9cfdb275c841aa0
im-mk <>
2025-10-04 20:17:51 +0100

    added menu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
-import './App.css'
-import OrderList from './pages/OrderList'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import OrderList from './pages/OrderList';
+import Layout from './layout/Layout';
 
 function App() {
-
   return (
-
-
-    <OrderList />
-
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/orders" element={<OrderList />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
